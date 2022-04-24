571a4a7acc13b18373cd2a4763b0abcab78b3144
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-04-23 21:34:35 -0400

    commit tree 28a00a75b18a385b7bb6c58dc3071e8252176be6

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,8 +1,9 @@
-class StatsController < ApplicationController
+class Admin::StatsController < ApplicationController
 
-  def index
-    @post_count = Post.count
-    @authors_count = Author.count
-    @last_post = Post.last
-  end
-end
+
+  def index	  def index
+    @post_count = Post.count	    @post_count = Post.count
+    @authors_count = Author.count	    @authors_count = Author.count
+    @last_post = Post.last	    @last_post = Post.last
+  end	  end
+end	end
